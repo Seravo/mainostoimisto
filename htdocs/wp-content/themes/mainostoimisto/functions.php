65d5e81b22c3d287748e41297dd5319a511c5b3c
Sami Keijonen <samik@seravo.com>
2025-09-07 11:01:31 +0300

    Preload fonts
    Preloading fonts helps performance and avoid FOUT.

--- htdocs/wp-content/themes/mainostoimisto/functions.php
+++ htdocs/wp-content/themes/mainostoimisto/functions.php
@@ -10,7 +10,7 @@
  *
  * @return void
  */
-function twentytwentyfive_enqueue_styles() {
+function mainostoimisto_enqueue_styles() {
 	wp_enqueue_style(
 		'mainostoimisto-style',
 		get_theme_file_uri( 'style.css' ),
@@ -18,4 +18,24 @@ function twentytwentyfive_enqueue_styles() {
 		wp_get_theme()->get( 'Version' )
 	);
 }
-add_action( 'wp_enqueue_scripts', 'twentytwentyfive_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'mainostoimisto_enqueue_styles' );
+
+/**
+ * Preload needed fonts in the head.
+ *
+ * @return void
+ */
+function mainostoimisto_preload_fonts() {
+	$theme_url = get_theme_file_uri();
+
+	$fonts = [
+		$theme_url . '/assets/fonts/fraunces/fraunces-v35-latin-700.woff2' => 'woff2',
+		$theme_url . '/assets/fonts/dm-sans/dm-sans-v15-latin-regular.woff2' => 'woff2',
+		$theme_url . '/assets/fonts/dm-sans/dm-sans-v15-latin-700.woff2' => 'woff2',
+	];
+
+	foreach ( $fonts as $font_link => $font_type ) {
+		echo '<link rel="preload" href="' . esc_url( $font_link ) . '" as="font" type="font/' . esc_attr( $font_type ) . '" crossorigin>';
+	}
+}
+add_action( 'wp_head', 'mainostoimisto_preload_fonts', 1 );
